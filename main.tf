c03829b775c169ff11cb4d528b6a7d9bf7f6a120
explodinghat <35501013+explodinghat@users.noreply.github.com>
2023-04-20 16:36:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,11 +12,11 @@ provider "azurerm" {
 }
 
 data "azurerm_resource_group" "rg" {
-  name = "[paste-existing-rg-here"
+  name = "[paste-existing-rg-here]"
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "[create-storage-acc-name-here"
+  name                     = "[create-storage-acc-name-here]"
   resource_group_name      = data.azurerm_resource_group.rg.name
   location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
