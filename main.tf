e4f39d547c33c5bd05c4c31228d7390770570a8e
explodinghat <35501013+explodinghat@users.noreply.github.com>
2023-04-20 16:36:04 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.52.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "[paste-existing-rg-here"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "[create-storage-acc-name-here"
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
